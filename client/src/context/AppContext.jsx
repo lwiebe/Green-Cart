f5e59b3353c9f967ec9b6353fe1f56e6b020d806
Lian Wiebe <72026264+lwiebe@users.noreply.github.com>
2025-04-16 16:52:31 +0200

    nav bar

--- client/src/context/AppContext.jsx
+++ client/src/context/AppContext.jsx
@@ -6,10 +6,11 @@ export const AppContext = createContext();
 export const AppContextProvider = ({ children })=>{
 
     const navigate = useNavigate();
-    const [user, setUser] = useState(null)
+    const [user, setUser] = useState(null);
     const [isSeller, setIsSeller] = useState(false)
+    const [showUserLogin, setShowUserLogin] = useState(false)
 
-    const value = {navigate, user, setUser, setIsSeller, isSeller}
+    const value = {navigate, user, setUser, setIsSeller, isSeller, showUserLogin, setShowUserLogin}
 
     return <AppContext.Provider value={value}>
         {children}
@@ -17,5 +18,5 @@ export const AppContextProvider = ({ children })=>{
 }
 
 export const useAppContext = ()=>{
-    useContext(AppContext)
+    return useContext(AppContext)
 }
