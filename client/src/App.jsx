e002fe0c7fe52d70c5471fb41d769a28ff1320c6
Lian Wiebe <72026264+lwiebe@users.noreply.github.com>
2025-04-19 14:34:45 +0200

    footer, full homepage

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,6 +4,7 @@ import MainBanner from './components/MainBanner'
 import { Route, Routes, useLocation } from 'react-router-dom'
 import Home from './pages/Home'
 import { Toaster } from 'react-hot-toast';
+import Footer from './components/Footer'
 
 function App() {
 
@@ -13,7 +14,7 @@ function App() {
     <div>
 
       {isSellerPath ? null : <Navbar />}
-      
+
       <Toaster />
       
       <div className={`${isSellerPath ? "" : "px-6 md:px-16 lg:px-24 xl:px-32"}`}>
@@ -21,6 +22,7 @@ function App() {
           <Route path='/' element={<Home />} />
         </Routes>
       </div>
+      {!isSellerPath && <Footer />}
     </div>
   )
 }
