9430cb5c32e675d5d79fee05cb619109609f3252
Lian Wiebe <72026264+lwiebe@users.noreply.github.com>
2025-04-19 14:03:40 +0200

    best seller section

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,6 +3,7 @@ import Navbar from './components/Navbar'
 import MainBanner from './components/MainBanner'
 import { Route, Routes, useLocation } from 'react-router-dom'
 import Home from './pages/Home'
+import { Toaster } from 'react-hot-toast';
 
 function App() {
 
@@ -12,6 +13,8 @@ function App() {
     <div>
 
       {isSellerPath ? null : <Navbar />}
+      
+      <Toaster />
       
       <div className={`${isSellerPath ? "" : "px-6 md:px-16 lg:px-24 xl:px-32"}`}>
         <Routes>
