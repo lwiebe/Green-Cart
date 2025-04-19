fa8444213d7d9419bbf0d47dadc31d7dcf678cb8
Lian Wiebe <72026264+lwiebe@users.noreply.github.com>
2025-04-19 16:05:12 +0200

    all products, search function

--- client/src/context/AppContext.jsx
+++ client/src/context/AppContext.jsx
@@ -16,6 +16,7 @@ export const AppContextProvider = ({ children })=>{
     const [products, setProducts] = useState([])
     
     const [cartItems, setCartItems] = useState({})
+    const [searchQuery, setSearchQuery] = useState({})
 
     // Fetch All Products
     const fetchProducts = async ()=>{
@@ -60,7 +61,7 @@ export const AppContextProvider = ({ children })=>{
         fetchProducts()
     },[])
 
-    const value = {navigate, user, setUser, setIsSeller, isSeller, showUserLogin, setShowUserLogin, products, setProducts, currency, addToCart, updateCartItem, removeFromCart, cartItems
+    const value = {navigate, user, setUser, setIsSeller, isSeller, showUserLogin, setShowUserLogin, products, setProducts, currency, addToCart, updateCartItem, removeFromCart, cartItems, searchQuery, setSearchQuery
     }
 
     return <AppContext.Provider value={value}>
