31469205b426693e9789cd86d6837ceb72010360
Lian Wiebe <72026264+lwiebe@users.noreply.github.com>
2025-04-20 18:38:06 +0200

    seller layout

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -14,6 +14,7 @@ import Cart from './pages/Cart'
 import AddAddress from './pages/AddAddress'
 import MyOrders from './pages/MyOrders'
 import SellerLogin from './components/seller/SellerLogin'
+import SellerLayout from './pages/seller/SellerLayout'
 
 function App() {
 
@@ -21,7 +22,7 @@ function App() {
   const {showUserLogin, isSeller} = useAppContext();
 
   return (
-    <div>
+    <div className='text-default min-h-screen text-gray-700 bg-white'>
 
       {isSellerPath ? null : <Navbar />}
       {showUserLogin ? <Login /> : null}
@@ -37,7 +38,7 @@ function App() {
           <Route path='/cart' element={<Cart />} />
           <Route path='/add-address' element={<AddAddress />} />
           <Route path='/my-orders' element={<MyOrders />} />
-          <Route path='/seller' element={isSeller ? null : <SellerLogin/>}>
+          <Route path='/seller' element={isSeller ? <SellerLayout/> : <SellerLogin/>}>
 
           </Route>
         </Routes>
