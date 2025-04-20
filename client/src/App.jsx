c8b25751584c8f37f720799efcf4d931069367c9
Lian Wiebe <72026264+lwiebe@users.noreply.github.com>
2025-04-20 11:21:08 +0200

    product category

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -8,6 +8,7 @@ import Footer from './components/Footer'
 import { useAppContext } from './context/AppContext'
 import Login from './components/Login'
 import AllProducts from './pages/AllProducts'
+import ProductCategory from './pages/ProductCategory'
 
 function App() {
 
@@ -26,6 +27,7 @@ function App() {
         <Routes>
           <Route path='/' element={<Home />} />
           <Route path='/products' element={<AllProducts />} />
+          <Route path='/products/:category' element={<ProductCategory />} />
         </Routes>
       </div>
       {!isSellerPath && <Footer />}
