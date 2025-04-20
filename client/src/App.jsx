edc415b3bcad7290c07bbe8287aa6033aa7bd367
Lian Wiebe <72026264+lwiebe@users.noreply.github.com>
2025-04-20 19:11:55 +0200

    add product page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -15,6 +15,9 @@ import AddAddress from './pages/AddAddress'
 import MyOrders from './pages/MyOrders'
 import SellerLogin from './components/seller/SellerLogin'
 import SellerLayout from './pages/seller/SellerLayout'
+import AddProduct from './pages/seller/AddProduct'
+import ProductList from './pages/seller/ProductList'
+import Orders from './pages/seller/Orders'
 
 function App() {
 
@@ -39,7 +42,9 @@ function App() {
           <Route path='/add-address' element={<AddAddress />} />
           <Route path='/my-orders' element={<MyOrders />} />
           <Route path='/seller' element={isSeller ? <SellerLayout/> : <SellerLogin/>}>
-
+            <Route index element={isSeller ? <AddProduct/> : null} />
+            <Route path='product-list' element={<ProductList/>} />
+            <Route path='orders' element={<Orders/>} />
           </Route>
         </Routes>
       </div>
